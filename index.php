8cbd5ec7a4261dffd869e3c3a960cba9862dc5a6
gjbr5 <wlwogud02@naver.com>
2019-03-14 15:50:01 +0900

    First Commit

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
+<meta charset="UTF-8"/>
+<html>
+<head>
+    <title>Hello!</title>
+</head>
+<body>
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 2019-03-14
- * Time: 오후 2:02
- */
 
-echo "HELLO WORLD!";
+echo "HELLO WORLD!<br/>\n";
+echo "안녕 세상!<br/>\n";
 
+$score = array(1, 2, 3);
+$score[10] = 0;
+$s = $score[0];
+echo $score[0]++;
+echo $s;
+sort($score);
+
+for ($x = 0; $x <= 10; $x++) {
+    for ($y = 0; $y <= $x; $y++) {
+        echo "*";
+    }
+    echo "<br/>\n";
+}
 ?>
+
+</body>
+</html>
